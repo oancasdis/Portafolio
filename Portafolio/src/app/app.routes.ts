720ff00fadb6de3f37043e7b930d8927e8bb44e2
oan <oancasdis@hotmail.com>
2024-08-14 00:26:53 -0400

    navbar y cv

--- Portafolio/src/app/app.routes.ts
+++ Portafolio/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./inicio/inicio.component').then(m => m.InicioComponent)  // Carga de InicioComponent como standalone
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
